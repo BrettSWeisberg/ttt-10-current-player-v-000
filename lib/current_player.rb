2cc5ffc8d634b440cc972647291040a6d22d61f2
Brett Weisberg <weisberg.brett@gmail.com>
2018-02-07 23:30:58 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,19 +1,4 @@
-#d ef turn_count(board)
-#  counter = 0
-#  board.each do |letters|
-#    counts[O] || counts[X]
-#   #board.count("X") || board.count("O")
-#   counter += 1
-#end
-#end
 
-#def current_player(current_player)
-#  if current_player % 2 == 0
-#    puts "X"
-#  else
-#    puts "O"
-#  end
-#end
 
 def turn_count(board)
  counter = 0
@@ -28,8 +13,8 @@ end
 def current_player(board)
   if turn_count(board) % 2 == 0
     #return "X" or
-    "X"
+    puts "X"
   else
-     "O"
+    puts "O"
   end
 end
